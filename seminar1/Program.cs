6ebc8894de663dceb268f6eed7555e47378276c6
Dmitry <dimakameya@gmail.com>
2023-01-13 22:39:15 +0500

    квадрат числа

---
+++ seminar1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+string numberStr = Console.ReadLine();
+int number = Convert.ToInt32(numberStr);
+
+int result = number * number;
+
+Console.WriteLine($"Квадрат числа {number} = {result}");
+Console.WriteLine("Квадрат числа " + number + " = " + result);
+Console.WriteLine("Квадрат числа {0} = {1}", number, result);
+Console.WriteLine("Квадрат числа {number} = {result}");

2499c93aa754e53212dabc3dee7b499504553b49
Dmitry <dimakameya@gmail.com>
2023-01-13 23:33:34 +0500

    poslednyaya cifra

---
+++ sem1_posledCifra/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Задача №7. Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает последнюю цифру этого числа.
+	456 -> 6
+	782 -> 2
+	918 -> 8
+*/
+
+Console.WriteLine("Введите трехзначное число: ");
+string numberStr = Console.ReadLine();
+int number = Convert.ToInt32(numberStr);
+int a = number % 10;
+Console.WriteLine(a);

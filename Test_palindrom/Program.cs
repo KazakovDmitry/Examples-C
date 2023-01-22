ce207cbc8e836899c6fd0d5e31e8441b1c82c6a7
Dmitry <dimakameya@gmail.com>
2023-01-22 13:58:50 +0500

    мой палиндром

---
+++ Test_palindrom/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Напишите программу, которая принимает на вход пятизначное 
+число и проверяет, является ли оно палиндромом.
+
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+
+Console.WriteLine("Введите пятизначное число: ");
+string numberA = Console.ReadLine();
+string numberB;
+
+numberB = numberA.Substring(numberA.Length - 1, 1);
+
+for (int i = 1; i < numberA.Length; i++)
+{
+    numberB = numberB + numberA.Substring(numberA.Length - i -1, 1);
+}
+
+Console.WriteLine(numberB);
+if(numberB == numberA) Console.WriteLine("Палиндром");
+else Console.WriteLine("Не палиндром");

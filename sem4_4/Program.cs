1b8d2e3db67a2a3fc3bf0c007e3915eb5a64942c
Dmitry <dimakameya@gmail.com>
2023-01-24 23:35:35 +0500

    Рандом

---
+++ sem4_4/Program.cs
@@ -0,0 +1,41 @@
+﻿/*Задача 30: Напишите программу, которая выводит массив из 8 элементов, 
+заполненный нулями и единицами в случайном порядке.
+[1,0,1,1,0,1,0,0]
+*/
+
+int [] RandomArray(int length)
+{
+    Random number = new Random();
+    int [] arr = new int [length];
+    for (int i = 0; i < length; i++)
+    {
+        arr [i] = number.Next(0, 2);
+        Console.Write($"{arr[i]} ");
+    }
+    return arr;
+
+}
+
+int a = new Random().Next(0,5)
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result) && result >= 1)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число. Повторите ввод");
+        }
+    }
+    return result;
+}
+
+int number = GetNumber("Введите длинну массива ");
+RandomArray(number);
